6445fd25cb9c365ca7f241e19cc565386340871a
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-02-16 13:40:27 -0800

    add both local and heroku deploys

---
+++ heroku_deployment/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/joannejordan/Desktop/LambdaSchool/GitHub/labs7_med_dash_deployment/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ local_deploy/rxvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/joannejordan/Desktop/LambdaSchool/med_dash_deploy/rxvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
