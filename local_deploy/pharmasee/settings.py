b54588dcaa7fe25de842c471ae6cf7514b24027a
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-03-08 07:23:36 -0800

    configure venv

--- local_deploy/pharmasee/settings.py
+++ local_deploy/pharmasee/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from .local_settings import secret_key, allowed_hosts
+from .local_settings import secret_key, allowed_hosts, user, password
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'pharmasee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rxdatabase',
+        'USER': user,
+        'PASSWORD': password,
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- local_deploy/rxvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/joannejordan/Desktop/LambdaSchool/med_dash_deploy/rxvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
