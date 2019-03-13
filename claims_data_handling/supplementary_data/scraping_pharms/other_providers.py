4306494d10e1d98d280f172d8c7318ef50160179
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-03-13 11:36:48 -0700

    add other_providers

--- claims_data_handling/supplementary_data/scraping_pharms/other_providers.py
+++ claims_data_handling/supplementary_data/scraping_pharms/other_providers.py
@@ -3,11 +3,12 @@ import sys
 import os
 import pandas as pd
 
-l = ['https://www.caremark.com/portal/asset/HCA90DayProviders.pdf',
-     'http://aidsnet.org/wp-content/uploads/2016/03/Expanded-Network-now-in-place-\
-for-ADAP-Premium-Plus-Clients.pdf',
-     'https://www.molinahealthcare.com/members/ny/en-US/PDF/EP/\
-MNY_Pharmacy_Network_2018.pdf',
-      'http://www.uhh.org/hubfs/Hospitality_Rx/_files/Welldyne-Network-Pharmacy-List.pdf']
+hca90 = 'https://www.caremark.com/portal/asset/HCA90DayProviders.pdf'
+aidsnet = 'http://aidsnet.org/wp-content/uploads/2016/03/Expanded-Network-now-in-place-\
+for-ADAP-Premium-Plus-Clients.pdf'
+molina = 'https://www.molinahealthcare.com/members/ny/en-US/PDF/EP/\
+MNY_Pharmacy_Network_2018.pdf'
+welldyne = 'http://www.uhh.org/hubfs/Hospitality_Rx/_files/Welldyne-Network-Pharmacy-\
+List.pdf']
 
-# Molina - start at 2nd page
+#  Molina - start at 2nd page
